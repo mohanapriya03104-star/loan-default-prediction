0f5fdeb94dc32e1e36b5acf19b9b8b78d1d25c95
mohanapriya03104-star <mohanapriya03104@gmail.com>
2026-04-23 16:03:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 
-# -------------------------------
+# ------------------------------
 # PAGE CONFIG
 # -------------------------------
 st.set_page_config(page_title="HDI Predictor", layout="centered")
